acc347464353e52708a2738c09cfc7f9fe120044
ManasaVemireddy <manasavemireddy9848@gmail.com>
2024-09-14 10:19:47 +0530

    Initial commit with React project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Button = (props) => {
+  const { className, buttonText } = props;
+  return <button className={className}>{buttonText}</button>;
+};
+
+const element = (
+  <div className="container1">
+    <h1 className="heading1">Social Buttons</h1>
+    <div className="containers">
+      <Button buttonText="Like" className="likeButton" />
+      <Button buttonText="Comment" className="commentButton" />
+      <Button buttonText="Share" className="shareButton" />
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById("root"));
